5653664b48edd622257c119af2d1effe8f1da124
MrJacob <46843555+MrJacob12@users.noreply.github.com>
2021-08-26 09:19:43 +0200

    1.3.1

--- src/page/AboutUs/AboutUs.js
+++ src/page/AboutUs/AboutUs.js
@@ -9,7 +9,9 @@ const AboutUs = () => {
 				<span>About us</span>
 			</Parallax>
 			<Parallax className="about-us__sub" y={["500px", "0px"]}>
-				<span>Hello, I only passing</span>
+				<p>Hi, we are a small team of programers that consists of a full stack and a frontend developer.</p>
+				<p>Fern.fun is a representation of our adventure with programing. We're an open team looking for new members.</p>
+				<p>If you want to join us or cooperate with us, we're glad to hear it, contact.us</p> 
 			</Parallax>
 			<Parallax>
 				<Fern height="500px" left="-150px" top="-150px" rotate="160deg" />
