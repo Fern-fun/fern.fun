23a9c73bb5621fb487ca5625399369158580eadc
MrJacob12 <jakub.buciuto@gmail.com>
2023-03-17 14:06:08 +0100

    6.0.0

--- src/Components/TeamContainer/Team.js
+++ src/Components/TeamContainer/Team.js
@@ -2,10 +2,25 @@ export const teams = [
   {
     name: "Jakub Buciuto",
     username: "MrJacob12",
-    email: "jakub.buciuto@gmail.com",
     github: "https://github.com/MrJacob12",
     linkedin: "https://www.linkedin.com/in/jakub-buciuto-4972bb21b/",
     avatar: "https://avatars.githubusercontent.com/u/46843555?v=4",
-    tags: ["python", "react", "rust", "typescript", "javascript"],
+    tags: ["python", "react", "rust", "typescript"],
+  },
+  {
+    name: "Bartłomiej Kudzia",
+    username: "QQLKAA",
+    github: "https://github.com/QQLKAA",
+    linkedin: "",
+    avatar: "https://avatars.githubusercontent.com/u/27441333?v=4",
+    tags: ["c", "c-plus-plus", "python", "react", "typescript"],
+  },
+  {
+    name: "Kacper Kostera",
+    username: "Skimper",
+    github: "https://github.com/Skimper",
+    linkedin: "",
+    avatar: "https://avatars.githubusercontent.com/u/42150605?v=4",
+    tags: ["c-sharp", "java", "react", "typescript"],
   },
 ];
--- src/Components/TeamContainer/TeamContainer.js
+++ src/Components/TeamContainer/TeamContainer.js
@@ -1,27 +1,59 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
 import { teams } from "./Team";
 
+import "./TeamContainer.scss";
+
 function TeamContainer() {
   return (
-    <div className="team-container">
-      {teams.map((team, index) => {
-        return (
-          <div className="team-container__team" key={index}>
-            <div className="team-container__team__avatar">
-              <img src={team.avatar} alt={team.name} />
+    <div className="team-container fadeInFromBottom">
+      <Helmet>
+        <title>About us</title>
+      </Helmet>
+      <div className="team-container__title">
+        <span className="fern-text">Our</span> team
+      </div>
+      <div className="team-container__content">
+        {teams.map((team, index) => {
+          return (
+            <div className="team-container__team" key={index}>
+              <div className="team-container__team__title">
+                <div className="team-container__team__avatar">
+                  <img src={team.avatar} alt={team.name} />
+                </div>
+                <div>
+                  <div className="team-container__team__username">
+                    {team.username}
+                  </div>
+                  <div className="team-container__team__name">{team.name}</div>
+                </div>
+              </div>
+              <div className="team-container__team_tags">
+                {team.tags.map((tag, index) => {
+                  return (
+                    <span className={`${tag} tag`} key={index}>
+                      #{tag.replaceAll("-plus", "+")}
+                    </span>
+                  );
+                })}
+              </div>
+              <div className="team-container__team__social">
+                {team.github ? (
+                  <a href={team.github} target="_blank" rel="noreferrer">
+                    <img src="/img/github.svg" width={28} />
+                  </a>
+                ) : null}
+                {team.linkedin ? (
+                  <a href={team.linkedin} target="_blank" rel="noreferrer">
+                    <img src="/img/linkedin.png" width={32} />
+                  </a>
+                ) : null}
+              </div>
             </div>
-            <div className="team-container__team__name">{team.name}</div>
-            <div className="team-container__team__username">
-              {team.username}
-            </div>
-
-            <div className="team-container__team__email">{team.email}</div>
-
-            <div className="team-container__team__social"></div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }
